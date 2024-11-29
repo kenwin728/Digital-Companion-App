df68c657ef5205fa861a51f3e359b94eb03ca719
kenwin728 <kenwin728@gmail.com>
2024-11-29 19:11:00 +0800

    added initial layout file

--- app/src/main/java/com/example/digitalcompanionapp/MainActivity.java
+++ app/src/main/java/com/example/digitalcompanionapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.digitalcompanionapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button enemyGridsBtn;
+    Button collectBtn;
+    Button buildBtn;
+    Button sabotageBtn;
+    TextView goldAmountText;
+    TextView woodAmountText;
+    TextView stoneAmountText;
+    TextView goldMineAmountText;
+    TextView treeReserveAmountText;
+    TextView quarryAmountText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +32,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        enemyGridsBtn = findViewById(R.id.btn_enemyGrids);
+        collectBtn = findViewById(R.id.btn_collect);
+        buildBtn = findViewById(R.id.btn_build);
+        sabotageBtn = findViewById(R.id.btn_sabotage);
+        goldAmountText = findViewById(R.id.goldAmountText);
+        woodAmountText = findViewById(R.id.woodAmountText);
+        stoneAmountText = findViewById(R.id.stoneAmountText);
+        goldMineAmountText = findViewById(R.id.goldMineAmountText);
+        treeReserveAmountText = findViewById(R.id.treeReserveAmountText);
+        quarryAmountText = findViewById(R.id.quarryAmountText);
+    }
+    public void enemyGrids(View v){
+
+    }
+    public void collect(View v){
+
+    }
+    public void build(View v){
+
+    }
+    public void sabotage(View v){
+
     }
 }
