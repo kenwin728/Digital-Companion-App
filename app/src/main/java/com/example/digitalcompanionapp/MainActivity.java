733b9b10197ab9aa423d1e560edc739ab6ae3b44
spoomplss <134779382+tempura9@users.noreply.github.com>
2024-11-29 23:43:19 +0800

    Added an error message, its not that great
    TELL ME WHY, AINT NOTHIN BUT A HEARTACHE

--- app/src/main/java/com/example/digitalcompanionapp/MainActivity.java
+++ app/src/main/java/com/example/digitalcompanionapp/MainActivity.java
@@ -27,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
     TextView goldMineAmountText;
     TextView treeReserveAmountText;
     TextView quarryAmountText;
+    TextView errorMessage;
     LinearLayout listOfResourcesLL;
+    LinearLayout listOfBuildOptionsLL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         });
         enemyGridsBtn = findViewById(R.id.btn_enemyGrids);
         collectBtn = findViewById(R.id.btn_collect);
-        buildBtn = findViewById(R.id.btn_build);
+        listOfBuildOptionsLL = findViewById(R.id.ll_listOfBuildingOptions);
         sabotageBtn = findViewById(R.id.btn_sabotage);
         gameNameText = findViewById(R.id.gameName);
         goldAmountText = findViewById(R.id.goldAmountText);
@@ -50,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         treeReserveAmountText = findViewById(R.id.treeReserveAmountText);
         quarryAmountText = findViewById(R.id.quarryAmountText);
         listOfResourcesLL = findViewById(R.id.ll_listOfResources);
+        errorMessage = findViewById(R.id.errorMessage);
 
         // Create a GradientDrawable for the border
         GradientDrawable borderDrawable = new GradientDrawable();
@@ -65,54 +68,110 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
     public void collect(View v){
-        View view;
         int building, resource;
         // Gold Collection
-        view = findViewById(R.id.goldMineAmountText);
-        building = 30 * Integer.parseInt(((TextView) view).getText().toString());
-        view = findViewById(R.id.goldAmountText);
-        resource = building + Integer.parseInt(((TextView) view).getText().toString());
-        ((TextView) view).setText(String.valueOf(resource));
+        building = 30 * Integer.parseInt(((TextView) goldMineAmountText).getText().toString());
+        resource = building + Integer.parseInt(((TextView) goldAmountText).getText().toString());
+        ((TextView) goldAmountText).setText(String.valueOf(resource));
 
         // Wood Collection
-        view = findViewById(R.id.treeReserveAmountText);
-        building = 25 * Integer.parseInt(((TextView) view).getText().toString());
-        view = findViewById(R.id.woodAmountText);
-        resource = building + Integer.parseInt(((TextView) view).getText().toString());
-        ((TextView) view).setText(String.valueOf(resource));
+        building = 25 * Integer.parseInt(((TextView) treeReserveAmountText).getText().toString());
+        resource = building + Integer.parseInt(((TextView) woodAmountText).getText().toString());
+        ((TextView) woodAmountText).setText(String.valueOf(resource));
 
-        // Wood Collection
-        view = findViewById(R.id.quarryAmountText);
-        building = 20 * Integer.parseInt(((TextView) view).getText().toString());
-        view = findViewById(R.id.stoneAmountText);
-        resource = building + Integer.parseInt(((TextView) view).getText().toString());
-        ((TextView) view).setText(String.valueOf(resource));
+        // Stone Collection
+        building = 20 * Integer.parseInt(((TextView) quarryAmountText).getText().toString());
+        resource = building + Integer.parseInt(((TextView) stoneAmountText).getText().toString());
+        ((TextView) stoneAmountText).setText(String.valueOf(resource));
+
+        errorMessage.setVisibility(View.GONE);
     }
-    public void build(View v){
-        //ill make the 4 buttons first
+    public void build_catapult(View v){
+        int gold, wood, stone;
+
+        gold = Integer.parseInt(((TextView) goldAmountText).getText().toString());
+        wood = Integer.parseInt(((TextView) woodAmountText).getText().toString());
+        stone = Integer.parseInt(((TextView) stoneAmountText).getText().toString());
+
+        if (gold < 50 || wood < 100 || stone < 70){
+            //a popup saying catapult cant be built pops up
+            errorMessage.setVisibility(View.VISIBLE);
+        } else {
+            gold -= 50;
+            wood -= 100;
+            stone -= 70;
+            ((TextView) goldAmountText).setText(String.valueOf(gold));
+            ((TextView) woodAmountText).setText(String.valueOf(wood));
+            ((TextView) stoneAmountText).setText(String.valueOf(stone));
+            errorMessage.setVisibility(View.GONE);
+        }
+    }
+
+    public void build_goldMine(View v){
+        int gold, goldMine_cnt;
+        gold = Integer.parseInt(((TextView) goldAmountText).getText().toString());
+
+        if (gold < 80){
+            //a popup saying goldmine cant be built pops up
+            errorMessage.setVisibility(View.VISIBLE);
+        } else {
+            gold -= 80;
+            ((TextView) goldAmountText).setText(String.valueOf(gold));
+            goldMine_cnt = 1 + Integer.parseInt(((TextView) goldMineAmountText).getText().toString());
+            ((TextView) goldMineAmountText).setText(String.valueOf(goldMine_cnt));
+            errorMessage.setVisibility(View.GONE);
+        }
+    }
+
+    public void build_treeReserve(View v){
+        int gold, treeReserve_cnt;
+        gold = Integer.parseInt(((TextView) goldAmountText).getText().toString());
+
+        if (gold < 60){
+            //a popup saying treereserve cant be built pops up
+            errorMessage.setVisibility(View.VISIBLE);
+        } else {
+            gold -= 60;
+            ((TextView) goldAmountText).setText(String.valueOf(gold));
+            treeReserve_cnt = 1 + Integer.parseInt(((TextView) treeReserveAmountText).getText().toString());
+            ((TextView) treeReserveAmountText).setText(String.valueOf(treeReserve_cnt));
+            errorMessage.setVisibility(View.GONE);
+        }
+    }
+
+    public void build_quarry(View v){
+        int gold, quarry_cnt;
+        gold = Integer.parseInt(((TextView) goldAmountText).getText().toString());
+
+        if (gold < 70){
+            //a popup saying quarry cant be built pops up
+            errorMessage.setVisibility(View.VISIBLE);
+        } else {
+            gold -= 70;
+            ((TextView) goldAmountText).setText(String.valueOf(gold));
+            quarry_cnt = 1 + Integer.parseInt(((TextView) quarryAmountText).getText().toString());
+            ((TextView) quarryAmountText).setText(String.valueOf(quarry_cnt));
+            errorMessage.setVisibility(View.GONE);
         }
     }
     public void sabotage(View v){
         int gold, wood, stone;
 
-        goldView = findViewById(R.id.goldAmountText);
-        gold = Integer.parseInt(((TextView) goldView).getText().toString());
-        woodView = findViewById(R.id.woodAmountText);
-        wood = Integer.parseInt(((TextView) woodView).getText().toString());
-        stoneView = findViewById(R.id.stoneAmountText);
-        stone = Integer.parseInt(((TextView) stoneView).getText().toString());
+        gold = Integer.parseInt(((TextView) goldAmountText).getText().toString());
+        wood = Integer.parseInt(((TextView) woodAmountText).getText().toString());
         stone = Integer.parseInt(((TextView) stoneAmountText).getText().toString());
 
         if (gold < 150 || wood < 150 || stone < 150){
             //a popup saying sabotage cant happen pops up
+            errorMessage.setVisibility(View.VISIBLE);
         } else {
             gold -= 150;
             wood -= 150;
             stone -= 150;
-            ((TextView) goldView).setText(String.valueOf(gold));
-            ((TextView) woodView).setText(String.valueOf(wood));
-            ((TextView) stoneView).setText(String.valueOf(stone));
+            ((TextView) goldAmountText).setText(String.valueOf(gold));
+            ((TextView) woodAmountText).setText(String.valueOf(wood));
             ((TextView) stoneAmountText).setText(String.valueOf(stone));
+            errorMessage.setVisibility(View.GONE);
         }
     }
 }
